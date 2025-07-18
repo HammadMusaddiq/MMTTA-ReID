471b5ef7094a005667a95a6ce79bcfbf054a430d
Muhammad Umair <muhammadumair894@gmail.com>
2025-07-18 21:54:55 +0800

    Update __init__.py

--- loss/__init__.py
+++ loss/__init__.py
@@ -1,3 +1,7 @@
 from .make_loss import make_loss, make_loss_ttt
 from .arcface import ArcFace
 # from .make_loss_ttt import make_loss_ttt
+from .vision_language_infonce import VisionLanguageInfoNCELoss
+from .distill_l2 import DistillL2Loss
+
+from .caption_adaptive_triplet import CaptionAdaptiveTripletLoss
